30afdfc60a234d21c93988e86d18964878a6178d
kento <KTC24A31A0043@edu.kyoto-tech.ac.jp>
2026-07-03 12:36:59 +0900

    Playerのあたり判定,ジャンプ,Animation

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,15 +3,27 @@ using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
-    [SerializeField] private float speed = 0f;
+    //[SerializeField] private float speed = 0f;
+    [SerializeField] private float acceleration = 0f;
     Vector2 inputVer;
+    [SerializeField] private float rotSpeed = 0f; 
+    private Vector3 targetRot;
 
     PlayerInput input;
     Rigidbody rb;
+    Animator animetor;
 
     public void OnMove(InputAction.CallbackContext context)
     {
         inputVer = context.ReadValue<Vector2>();
+    }
+
+    public void OnJump(InputAction.CallbackContext context)
+    {
+        if (context.started)
+        {
+            rb.AddForce(Vector3.up * 5, ForceMode.Impulse);
+        }
     }
 
 
@@ -20,8 +32,9 @@ public class Player : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        //rb = GetComponent<Rigidbody>();
+        rb = GetComponent<Rigidbody>();
         input = GetComponent<PlayerInput>();
+        animetor = GetComponentInChildren<Animator>();
     }
 
     // Update is called once per frame
@@ -29,8 +42,20 @@ public class Player : MonoBehaviour
     {
         var cameraDir = input.camera.transform.forward;
         var cameraRigth = input.camera.transform.right;
-        Vector3 move = (cameraRigth.normalized * inputVer.x + cameraDir.normalized * inputVer.y) * speed * Time.deltaTime;
+        Vector3 move = (cameraRigth.normalized * inputVer.x + cameraDir.normalized * inputVer.y ) * acceleration;
         move.y = 0;
-        transform.position += move;
+        //transform.position += move;
+        rb.AddForce(move, ForceMode.Acceleration);
+        //rb.linearVelocity += move;
+        if (move != Vector3.zero)
+        {
+            targetRot = move.normalized;
+        }
+        Vector3 fowerd = transform.forward;
+        transform.up = Vector3.up;
+        transform.forward = Vector3.Slerp(fowerd, targetRot, rotSpeed * Time.deltaTime);
+
+        float mag = rb.linearVelocity.magnitude;
+        animetor.SetFloat("Speed", mag);
     }
 }
